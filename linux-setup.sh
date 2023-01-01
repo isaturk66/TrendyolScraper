aab30557ab3cf1c9ec42e79e688ba50200589907
Isa Turk <isaturk07@gmail.com>
2023-01-01 21:31:35 +0800

    Chromedriver update

--- linux-setup.sh
+++ linux-setup.sh
@@ -1,5 +1,5 @@
 sudo apt install python3-pip
 sudo pip3 install -r requirements.txt
-sudo apt-get update # to update ubuntu to correctly run apt install
-sudo apt install chromium-chromedriver
-sudo cp /usr/lib/chromium-browser/chromedriver /usr/bin
+#sudo apt-get update # to update ubuntu to correctly run apt install
+#sudo apt install chromium-chromedriver
+#sudo cp /usr/lib/chromium-browser/chromedriver /usr/bin
